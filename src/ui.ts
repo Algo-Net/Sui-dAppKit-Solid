f1e1e2ff8bcf61e27b196a7f2e16f3b46f771146
Ocky <ocky@algonet.xyz>
2026-09-13 14:51:05 +0000

    Imported everything that should be exported.

--- src/ui.ts
+++ src/ui.ts
@@ -1,3 +1,7 @@
 import "./ui.css";
 
-export const greet = (name: string): string => `Hello ${name}`;
+export type { ConnectButtonProps } from "src/components/ConnectButton";
+export { ConnectButton } from "src/components/ConnectButton";
+
+export type { ConnectModalProps } from "src/components/ConnectModal";
+export { ConnectModal } from "src/components/ConnectModal";
